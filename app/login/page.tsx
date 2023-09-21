8430436fc4aaef127719b5fe4027f1f17f6fe4f6
Alex Estevez <aestevez@college.harvard.edu>
2023-09-21 17:38:41 -0400

    trivial changes

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -17,8 +17,8 @@ export default async function LoginPage() {
   return (
     <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
       <div className="flex flex-col space-y-2 text-center">
-        <h1 className="text-2xl font-semibold tracking-tight">Sign up/log in</h1>
-        <p className="text-sm text-muted-foreground">Enter your email below to sign in or create a new account</p>
+        <h1 className="text-2xl font-semibold tracking-tight">Sign up or log in</h1>
+        <p className="text-sm text-muted-foreground">Enter your login information, or create a new account</p>
       </div>
       <UserAuthForm />
     </div>
