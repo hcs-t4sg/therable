a1b0ebde3aaf90f72e3bf6e7d4c2b71382c494ba
Alex Estevez <aestevez@college.harvard.edu>
2023-10-17 17:48:47 -0400

    Removed RLS and policies for now

--- setup.sql
+++ setup.sql
@@ -38,7 +38,7 @@ create table admins (
 -- Create a table for clinics - this was being problematic when putting it into supabse
 create table clinics (
   id uuid not null primary key,
-  owner references clinicians(id) not null,
+  owner uuid references clinicians(id) not null,
   code text not null,
   name text not null
 );
@@ -123,76 +123,75 @@ create trigger on_auth_user_created
   after insert on auth.users
   for each row execute procedure public.handle_new_user();
 
--- Enable Row Level Security for patients, clinicians, admins, clinics, messages, tasks, clinicMembers, and milestones tables
-ALTER TABLE patients ENABLE ROW LEVEL SECURITY;
-ALTER TABLE clinicians ENABLE ROW LEVEL SECURITY;
-ALTER TABLE admins ENABLE ROW LEVEL SECURITY;
-ALTER TABLE clinics ENABLE ROW LEVEL SECURITY;
-ALTER TABLE messages ENABLE ROW LEVEL SECURITY;
-ALTER TABLE tasks ENABLE ROW LEVEL SECURITY;
-ALTER TABLE clinicMembers ENABLE ROW LEVEL SECURITY;
-ALTER TABLE milestones ENABLE ROW LEVEL SECURITY;
+-- -- Enable Row Level Security for patients, clinicians, admins, clinics, messages, tasks, clinicMembers, and milestones tables
+-- ALTER TABLE patients ENABLE ROW LEVEL SECURITY;
+-- ALTER TABLE clinicians ENABLE ROW LEVEL SECURITY;
+-- ALTER TABLE admins ENABLE ROW LEVEL SECURITY;
+-- ALTER TABLE clinics ENABLE ROW LEVEL SECURITY;
+-- ALTER TABLE messages ENABLE ROW LEVEL SECURITY;
+-- ALTER TABLE tasks ENABLE ROW LEVEL SECURITY;
+-- ALTER TABLE clinicMembers ENABLE ROW LEVEL SECURITY;
+-- ALTER TABLE milestones ENABLE ROW LEVEL SECURITY;
 
--- Allow users to view/edit their own data in the patients, clinicians, and admins tables
-CREATE POLICY "Users can view/ edit their own data."
-AS PERMISSIVE
-FOR SELECT, UPDATE
-USING (auth.uid() = userId);
+-- -- Allow users to view/edit their own data in the patients, clinicians, and admins tables
+-- create policy "Users can view/ edit their own data."
+-- on users for select, update
+-- using (auth.uid() = userId);
 
--- Allow clinicians to view patient data
-CREATE POLICY "Clinicians can view patient data."
-AS PERMISSIVE
-FOR SELECT
-USING (auth.uid() = userId OR (auth.has_role('clinician') AND auth.uid() = (SELECT userId FROM patients WHERE id = current.id)));
+-- -- Allow clinicians to view patient data
+-- CREATE POLICY "Clinicians can view patient data."
+-- AS PERMISSIVE
+-- FOR SELECT
+-- USING (auth.uid() = userId OR (auth.has_role('clinician') AND auth.uid() = (SELECT userId FROM patients WHERE id = current.id)));
 
--- Allow clinicians to create/edit/delete/view their clinics
-CREATE POLICY "Clinicians can create/ edit/ delete their clinics."
-AS PERMISSIVE
-FOR INSERT, UPDATE, DELETE, SELECT
-USING (auth.uid() = userId AND current.id = (SELECT clinicId FROM clinics WHERE id = current.id));
+-- -- Allow clinicians to create/edit/delete/view their clinics
+-- CREATE POLICY "Clinicians can create/ edit/ delete their clinics."
+-- AS PERMISSIVE
+-- FOR INSERT, UPDATE, DELETE, SELECT
+-- USING (auth.uid() = userId AND current.id = (SELECT clinicId FROM clinics WHERE id = current.id));
 
--- Allow users to view/insert their messages (whether they are sender or receiver)
-CREATE POLICY "Users can view/ insert their messages."
-AS PERMISSIVE
-FOR SELECT, INSERT
-USING (auth.uid() = sender OR auth.uid() = receiver);
+-- -- Allow users to view/insert their messages (whether they are sender or receiver)
+-- CREATE POLICY "Users can view/ insert their messages."
+-- AS PERMISSIVE
+-- FOR SELECT, INSERT
+-- USING (auth.uid() = sender OR auth.uid() = receiver);
 
--- Allow patients to view/edit tasks they have been assigned
-CREATE POLICY "Patients can view/ edit assigned tasks."
-AS PERMISSIVE
-FOR SELECT, UPDATE
-USING (auth.uid() = userId);
+-- -- Allow patients to view/edit tasks they have been assigned
+-- CREATE POLICY "Patients can view/ edit assigned tasks."
+-- AS PERMISSIVE
+-- FOR SELECT, UPDATE
+-- USING (auth.uid() = userId);
 
--- Allow clinicians to create/edit/delete/view tasks they have assigned
-CREATE POLICY "Clinicians can create/ edit/ delete/ view assigned tasks."
-AS PERMISSIVE
-FOR INSERT, UPDATE, DELETE, SELECT
-USING (auth.uid() = assigner);
+-- -- Allow clinicians to create/edit/delete/view tasks they have assigned
+-- CREATE POLICY "Clinicians can create/ edit/ delete/ view assigned tasks."
+-- AS PERMISSIVE
+-- FOR INSERT, UPDATE, DELETE, SELECT
+-- USING (auth.uid() = assigner);
 
--- Allow patients to view milestones they have been assigned
-CREATE POLICY "Patients can view milestones they have been assigned"
-AS PERMISSIVE
-FOR SELECT
-USING (auth.uid() = userId);
+-- -- Allow patients to view milestones they have been assigned
+-- CREATE POLICY "Patients can view milestones they have been assigned"
+-- AS PERMISSIVE
+-- FOR SELECT
+-- USING (auth.uid() = userId);
 
--- Allow clinicians to create/edit/delete/view milestones they have assigned
-CREATE POLICY "Clinicians can create/ edit/ delte/ view assign milestones."
-AS PERMISSIVE
-FOR INSERT, UPDATE, DELETE, SELECT
-USING (auth.uid() = assigner);
+-- -- Allow clinicians to create/edit/delete/view milestones they have assigned
+-- CREATE POLICY "Clinicians can create/ edit/ delte/ view assign milestones."
+-- AS PERMISSIVE
+-- FOR INSERT, UPDATE, DELETE, SELECT
+-- USING (auth.uid() = assigner);
 
--- Create policy for clinic members to view clinics they belong to
-CREATE POLICY clinic_members_access_policy
-AS PERMISSIVE
-FOR SELECT
-USING (auth.uid() = userId OR auth.has_role('clinician'));
+-- -- Create policy for clinic members to view clinics they belong to
+-- CREATE POLICY clinic_members_access_policy
+-- AS PERMISSIVE
+-- FOR SELECT
+-- USING (auth.uid() = userId OR auth.has_role('clinician'));
 
--- Apply RLS policies to each respective table
-ALTER TABLE patients ENABLE ROW LEVEL SECURITY;
-ALTER TABLE clinicians ENABLE ROW LEVEL SECURITY;
-ALTER TABLE admins ENABLE ROW LEVEL SECURITY;
-ALTER TABLE clinics ENABLE ROW LEVEL SECURITY;
-ALTER TABLE messages ENABLE ROW LEVEL SECURITY;
-ALTER TABLE tasks ENABLE ROW LEVEL SECURITY;
-ALTER TABLE clinicMembers ENABLE ROW LEVEL SECURITY;
-ALTER TABLE milestones ENABLE ROW LEVEL SECURITY;
+-- -- Apply RLS policies to each respective table
+-- ALTER TABLE patients ENABLE ROW LEVEL SECURITY;
+-- ALTER TABLE clinicians ENABLE ROW LEVEL SECURITY;
+-- ALTER TABLE admins ENABLE ROW LEVEL SECURITY;
+-- ALTER TABLE clinics ENABLE ROW LEVEL SECURITY;
+-- ALTER TABLE messages ENABLE ROW LEVEL SECURITY;
+-- ALTER TABLE tasks ENABLE ROW LEVEL SECURITY;
+-- ALTER TABLE clinicMembers ENABLE ROW LEVEL SECURITY;
+-- ALTER TABLE milestones ENABLE ROW LEVEL SECURITY;
