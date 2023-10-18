cb9f61f016cc53795aaeb8596c9b6e5c4a743e69
isabelkim <isabel_kim@college.harvard.edu>
2023-10-17 20:26:01 -0400

    Clinics

--- app/create-clinic/page.tsx
+++ app/create-clinic/page.tsx
@@ -2,26 +2,22 @@ import { createServerSupabaseClient } from "@/lib/server-utils";
 import { redirect } from "next/navigation";
 import CreateClinicForm from "./create-clinic-form";
 
-export default async function LoginPage() {
+export default async function CreateClinicPage() {
   // Create supabase server component client and obtain user session from stored cookie
   const supabase = createServerSupabaseClient();
   const {
     data: { session },
   } = await supabase.auth.getSession();
 
-//   if (session) {
-//     // Users who are already signed in should be redirected to species page
-//     redirect("/species");
-//   }
-//   else {
-//     console.log(session)
-//   }
+  if (!session) {
+    redirect("/login");
+  }
 
   return (
     <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
       <div className="flex flex-col space-y-2 text-center">
-        <h1 className="text-2xl font-semibold tracking-tight">Sign Up / Log In</h1>
-        <p className="text-sm text-muted-foreground">Enter your email below to sign in or create a new account.</p>
+        <h1 className="text-2xl font-semibold tracking-tight">Create Clinic</h1>
+        <p className="text-sm text-muted-foreground">Create a clinic</p>
       </div>
       <CreateClinicForm />
     </div>
