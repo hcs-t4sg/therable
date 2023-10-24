6783d383571608ddf50fc438bc1991ad40d80185
Alex Estevez <aestevez@college.harvard.edu>
2023-10-24 19:58:48 -0400

    Adjusted single name input to function without useForm

--- app/create-clinic/create-clinic-form.tsx
+++ app/create-clinic/create-clinic-form.tsx
@@ -11,105 +11,102 @@ export default function CreateClinicForm(clinician: Clinician) {
 
   const supabaseClient = createClientComponentClient<Database>();
 
-  type FormData = {
-    clinicName: string;
+  // type FormData = {
+  //   clinicName: string;
+  // };
+
+  // const CreateClinicForm: React.FC = () => {
+  //   const {
+  //     register,
+  //     handleSubmit,
+  //     formState: { errors },
+  //   } = useForm<FormData>();
+  // const [successMessage, setSuccessMessage] = useState<string | null>(null);
+  // const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [clinicName, setClinicName] = useState<string>("")
+
+  async function generateUniqueCode() {
+    const codeLength = 16;
+    const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()';
+
+    while (true) {
+      let code = '';
+      for (let i = 0; i < codeLength; i++) {
+          code += characters.charAt(Math.floor(Math.random() * characters.length));
+      }
+
+      // Query the Supabase database to check for the existence of the code
+      const { data, error } = await supabaseClient
+          .from('clinics')
+          .select('id')
+          .eq('code', code);
+
+      if (error) {
+          throw error;
+      }
+
+      if (!data || data.length === 0) {
+          return code;
+      }
+    }
   };
 
-  const CreateClinicForm: React.FC = () => {
-    const {
-      register,
-      handleSubmit,
-      formState: { errors },
-    } = useForm<FormData>();
-    const [isLoading, setIsLoading] = useState(false);
-    const [successMessage, setSuccessMessage] = useState<string | null>(null);
-    const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const onSubmit = async (name: string) => {
+    setIsLoading(true);
 
-    async function generateUniqueCode() {
-      const codeLength = 16;
-      const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()';
+    try {
+        // Generate a unique clinic code
+        const clinicCode = await generateUniqueCode();
 
-      while (true) {
-        let code = '';
-        for (let i = 0; i < codeLength; i++) {
-            code += characters.charAt(Math.floor(Math.random() * characters.length));
-        }
+        // Define the data to be inserted
+        const clinicData = {
+          name,
+          code: clinicCode,
+          owner: clinician.id,
+        };
 
-        // Query the Supabase database to check for the existence of the code
-        const { data, error } = await supabaseClient
-            .from('clinics')
-            .select('id')
-            .eq('code', code);
+        // Insert the clinic data into the 'clinics' table
+        const { data: clinic, error } = await supabaseClient
+          .from("clinics")
+          .upsert(clinicData);
 
         if (error) {
-            throw error;
+          setErrorMessage(error.message);
+        } else {
+          setSuccessMessage(`Your clinic code: ${clinicCode}`);
         }
-
-        if (!data || data.length === 0) {
-            return code;
-        }
+      } catch (error) {
+        setErrorMessage((error as Error).message);
+      } finally {
+        setIsLoading(false);
       }
-    };
-
-    const onSubmit = async (data: FormData) => {
-      // if (!userIsClinician) {
-      //     // Handle the case where the user is not a clinician
-      //     redirect("/dashboard");
-      //   }
-
-      setIsLoading(true);
-
-      try {
-          // Generate a unique clinic code
-          const clinicCode = await generateUniqueCode();
-
-          // Define the data to be inserted
-          const clinicData = {
-            name: data.clinicName,
-            code: clinicCode,
-            owner: clinician.id,
-          };
-
-          // Insert the clinic data into the 'clinics' table
-          const { data: clinic, error } = await supabaseClient
-            .from("clinics")
-            .upsert(clinicData);
-
-          if (error) {
-            setErrorMessage(error.message);
-          } else {
-            setSuccessMessage(`Your clinic code: ${clinicCode}`);
-          }
-        } catch (error) {
-          setErrorMessage((error as Error).message);
-        } finally {
-          setIsLoading(false);
-        }
-    };
-
-    return (
-      <div>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <div>
-              <label htmlFor="clinicName">Clinic Name</label>
-              <input
-                id="clinicName"
-                type="text"
-                autoComplete="off"
-                {...register('clinicName')}
-              />
-              {errors.clinicName && <p>{errors.clinicName.message}</p>}
-            </div>
-            <button type="submit" disabled={isLoading}>
-              Create Clinic
-            </button>
-          </form>
-
-        {/* {successMessage && <p>{successMessage}</p>}
-        {errorMessage && <p>{errorMessage}</p>} */}
-      </div>
-    );
   };
-}
+
+  return (
+    <div>
+      <form onSubmit={() => void onSubmit(clinicName)}>
+          <div>
+            <label htmlFor="clinicName">Clinic Name</label>
+            <input
+              id="clinicName"
+              type="text"
+              autoComplete="off"
+              onChange={(e) => setClinicName(e.target.value)}
+            />
+            {/* {errors.clinicName && <p>{errors.clinicName.message}</p>} */}
+          </div>
+          <button type="submit" disabled={isLoading}>
+            Create Clinic
+          </button>
+        </form>
+
+      {/* {successMessage && <p>{successMessage}</p>}
+      {errorMessage && <p>{errorMessage}</p>} */}
+    </div>
+  );
+};
+
 
 
