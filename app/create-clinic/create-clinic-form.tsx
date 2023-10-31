cb60792c09a706e8402c4a39a4715ad591408396
Alex Estevez <aestevez@college.harvard.edu>
2023-10-31 16:45:56 -0400

    error message for empty name field

--- app/create-clinic/create-clinic-form.tsx
+++ app/create-clinic/create-clinic-form.tsx
@@ -1,13 +1,11 @@
 "use client";
 
-import React, { useState } from "react";
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { type Database } from "@/lib/schema";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useState } from "react";
 type Clinician = Database["public"]["Tables"]["clinicians"]["Row"];
 
-
 export default function CreateClinicForm(clinician: Clinician) {
-
   const supabaseClient = createClientComponentClient<Database>();
 
   // type FormData = {
@@ -20,97 +18,83 @@ export default function CreateClinicForm(clinician: Clinician) {
   //     handleSubmit,
   //     formState: { errors },
   //   } = useForm<FormData>();
-  const [successMessage, setSuccessMessage] = useState<string>("");
-  const [errorMessage, setErrorMessage] = useState<string>("");
+  const [message, setMessage] = useState<string | null>(null);
 
   const [isLoading, setIsLoading] = useState(false);
-  const [clinicName, setClinicName] = useState<string>("")
+  const [clinicName, setClinicName] = useState<string>("");
 
   async function generateUniqueCode() {
     const codeLength = 16;
-    const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()';
+    const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
 
     // eslint-disable-next-line no-constant-condition
     while (true) {
-      let code = '';
+      let code = "";
       for (let i = 0; i < codeLength; i++) {
-          code += characters.charAt(Math.floor(Math.random() * characters.length));
+        code += characters.charAt(Math.floor(Math.random() * characters.length));
       }
 
       // Query the Supabase database to check for the existence of the code
-      const { data, error } = await supabaseClient
-          .from('clinics')
-          .select('id')
-          .eq('code', code);
+      const { data, error } = await supabaseClient.from("clinics").select("id").eq("code", code);
 
       if (error) {
-          throw error;
+        throw error;
       }
 
       if (!data || data.length === 0) {
-          return code;
+        return code;
       }
     }
-  };
+  }
 
   const onSubmit = async (name: string) => {
     setIsLoading(true);
-    if (!name){
-      setErrorMessage("Clinic name cannot be empty.")
-      return
-    }
 
     try {
-        // Generate a unique clinic code
-        const clinicCode = await generateUniqueCode();
+      // Generate a unique clinic code
+      const clinicCode = await generateUniqueCode();
 
-        // Define the data to be inserted
-        const clinicData = {
-          name,
-          code: clinicCode,
-          owner: clinician.id,
-        };
-
-        // Insert the clinic data into the 'clinics' table
-        const { error } = await supabaseClient
-          .from("clinics")
-          .upsert(clinicData);
-
-        if (error) {
-          setErrorMessage(error.message);
-        } else {
-          setSuccessMessage(`Your clinic code: ${clinicCode}`);
-        }
-      } catch (error) {
-        setErrorMessage((error as Error).message);
-      } finally {
-        setIsLoading(false);
+      // putting this before line 55 refreshes page for some reason; marked for fixing later
+      if (!name) {
+        throw Error("Clinic name cannot be empty")
       }
+
+      // Define the data to be inserted
+      const clinicData = {
+        name,
+        code: clinicCode,
+        owner: clinician.id,
+      };
+
+      // Insert the clinic data into the 'clinics' table
+      const { error } = await supabaseClient.from("clinics").upsert(clinicData);
+
+      if (error) {
+        setMessage(error.message);
+      } else {
+        setMessage(`Your clinic code: ${clinicCode}`);
+      }
+    } catch (error) {
+      setMessage((error as Error).message);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
     <div>
       <form>
-          <div>
-            <label htmlFor="clinicName">Clinic Name</label>
-            <input
-              id="clinicName"
-              type="text"
-              autoComplete="off"
-              onChange={(e) => setClinicName(e.target.value)}
-            />
-            {/* {errors.clinicName && <p>{errors.clinicName.message}</p>} */}
-          </div>
-          <button onClick={() => void onSubmit(clinicName)} disabled={isLoading}>
-            Create Clinic
-          </button>
-        </form>
+        <div>
+          <label htmlFor="clinicName">Clinic Name</label>
+          <input id="clinicName" type="text" autoComplete="off" onChange={(e) => setClinicName(e.target.value)} />
+          {/* {errors.clinicName && <p>{errors.clinicName.message}</p>} */}
+        </div>
+        <button onClick={() => void onSubmit(clinicName)} disabled={isLoading}>
+          Create Clinic
+        </button>
+      </form>
 
-      {successMessage && <p>{successMessage}</p>}
-      {errorMessage && <p>{errorMessage}</p>}
+      {message && <p>{message}</p>}
     </div>
   );
-};
-
-
-
+}
