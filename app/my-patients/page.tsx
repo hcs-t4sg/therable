3bf491c148a12367801eae340db309d8537da494
Alex Estevez <aestevez@college.harvard.edu>
2023-11-05 17:17:50 -0500

    Added "My patients" text

--- app/my-patients/page.tsx
+++ app/my-patients/page.tsx
@@ -24,27 +24,29 @@ export default async function MyPatients() {
     .select(`*, patients(first_name, last_name)`)
     .eq("clinic_id", clinic?.id);
 
-  // console.log('CLINIC: ', clinic, 'PATIENT: ', patients)
   return (
-    <div className="grid grid-cols-4 gap-4">
-      {patients ? (
-        patients.map((patient, i) => (
-          <PatientCard
-            key={i}
-            userId={patient.id}
-            firstName={patient.patients ? patient.patients.first_name : 'FNU'}
-            lastName={patient.patients ? patient.patients.last_name : 'LNU'}
-            diagnosis={patient.diagnosis}
-            lastVisit={new Date()}
-            nextVisit={new Date()}
-            avatar="https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-          />
-        ))
-      ) : clinic ? (
-        <p>No patients currently enrolled</p>
-      ) : (
-        <p>You haven&apos;t created a clinic yet!</p>
-      )}
-    </div>
+    <>
+      <p className="text-2xl mb-10">My patients</p>
+      <div className="grid grid-cols-4 gap-4">
+        {patients ? (
+          patients.map((patient, i) => (
+            <PatientCard
+              key={i}
+              userId={patient.id}
+              firstName={patient.patients ? patient.patients.first_name : "FNU"}
+              lastName={patient.patients ? patient.patients.last_name : "LNU"}
+              diagnosis={patient.diagnosis}
+              lastVisit={new Date()}
+              nextVisit={new Date()}
+              avatar="https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+            />
+          ))
+        ) : clinic ? (
+          <p>No patients currently enrolled</p>
+        ) : (
+          <p>You haven&apos;t created a clinic yet!</p>
+        )}
+      </div>
+    </>
   );
 }
