9d9c55d80ab44a002588ffbc0c543fd91e678f5f
Taj Jethwani-Keyser <tjethwanikeyser@college.harvard.edu>
2023-11-04 22:27:35 -0400

    removed all radix themes references

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
 import { ModeToggle } from "@/app/(components-navbar)/mode-toggle";
 import { Toaster } from "@/components/ui/toaster";
-import { Theme } from "@radix-ui/themes";
-import "@radix-ui/themes/styles.css";
 import AuthStatus from "./(components-navbar)/auth-status";
 import Navbar from "./(components-navbar)/navbar";
 import "./globals.css";
@@ -17,28 +15,26 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en" suppressHydrationWarning>
       {/* Hydration warning suppressed because of next-themes https://github.com/pacocoursey/next-themes */}
       <body>
-        <Theme>
-          <Providers>
-            <div className="flex-col md:flex">
-              <div className="border-b">
-                <div className="flex h-16 items-center px-4">
-                  <Navbar className="mx-6" />
-                  <div className="ml-auto flex items-center space-x-4">
-                    <ModeToggle />
-                    <AuthStatus />
-                  </div>
+        <Providers>
+          <div className="flex-col md:flex">
+            <div className="border-b">
+              <div className="flex h-16 items-center px-4">
+                <Navbar className="mx-6" />
+                <div className="ml-auto flex items-center space-x-4">
+                  <ModeToggle />
+                  <AuthStatus />
                 </div>
               </div>
-              {/* Conditionally display website if logged in, else display login page */}
-              {/* <div className="flex-1 space-y-4 p-8 pt-6"> */}
-              <div className="space-y-6 p-10 pb-16 md:block">
-                <main>{children}</main>
-              </div>
-              {/* </div> */}
             </div>
-          </Providers>
-          <Toaster />
-        </Theme>
+            {/* Conditionally display website if logged in, else display login page */}
+            {/* <div className="flex-1 space-y-4 p-8 pt-6"> */}
+            <div className="space-y-6 p-10 pb-16 md:block">
+              <main>{children}</main>
+            </div>
+            {/* </div> */}
+          </div>
+        </Providers>
+        <Toaster />
       </body>
     </html>
   );
