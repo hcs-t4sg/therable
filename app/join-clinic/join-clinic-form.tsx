c20274c48e99f3733595011ab522a3c8209e6829
isabelkim <isabel_kim@college.harvard.edu>
2023-11-28 22:46:51 -0500

    Join clinic issues

--- app/join-clinic/join-clinic-form.tsx
+++ app/join-clinic/join-clinic-form.tsx
@@ -2,10 +2,11 @@
 
 import { type Database } from "@/lib/schema";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 type Patient = Database["public"]["Tables"]["patients"]["Row"];
 
 // CF&Y@cHWrxI$z8p3
+// ILR clinic eCV7OAP2jiOQzR%m
 
 export default function JoinClinicForm(patient: Patient) {
   const supabaseClient = createClientComponentClient<Database>();
@@ -19,10 +20,53 @@ export default function JoinClinicForm(patient: Patient) {
   const [clinicId, setClinicId] = useState<string>("");
   // const [confirmation, setConfirmation] = useState<boolean>(false);
 
+  useEffect(() => {
+    const joinClinic = async () => {
+      try {
+        // Check if the patient is already a member of the clinic
+        const existingMembership = await supabaseClient
+          .from("clinic_members")
+          .select("clinic_id")
+          .eq("patient_id", patient.id)
+          .eq("clinic_id", clinicId);
+
+        if (existingMembership.data && existingMembership.data.length > 0) {
+          setErrorMessage("You are already a member of this clinic.");
+          return;
+        }
+
+        // Define the data to be inserted
+        const clinicMemberData = {
+          patient_id: patient.id,
+          clinic_id: clinicId,
+          diagnosis: "",
+        };
+
+        // Insert the clinic data into the 'clinic_members' table
+        const { error } = await supabaseClient.from("clinic_members").upsert(clinicMemberData);
+
+        if (error) {
+          setErrorMessage(error.message);
+        } else {
+          setSuccessMessage(`Joined clinic: ${clinicName}`);
+        }
+      } catch (error) {
+        setErrorMessage((error as Error).message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    if (clinicId) {
+      joinClinic();
+    }
+  }, [clinicId, clinicName, patient.id, supabaseClient]);
+
   const onSubmit = async (code: string) => {
     setIsLoading(true);
     if (!code) {
       setErrorMessage("Please enter a clinic code");
+      setIsLoading(false);
       return;
     }
 
@@ -33,34 +77,11 @@ export default function JoinClinicForm(patient: Patient) {
     } else if (data && data.length > 0 && data[0]) {
       setClinicName(data[0].name);
       setClinicId(data[0].id);
-      // setConfirmation(true);
     } else {
       setErrorMessage("Could not find a clinic with this code.");
     }
 
     setIsLoading(false);
-
-    try {
-      // Define the data to be inserted
-      const clinicMemberData = {
-        patient_id: patient.id,
-        clinic_id: clinicId,
-        diagnosis: "",
-      };
-
-      // Insert the clinic data into the 'clinics' table
-      const { error } = await supabaseClient.from("clinic_members").upsert(clinicMemberData);
-
-      if (error) {
-        setErrorMessage(error.message);
-      } else {
-        setSuccessMessage(`Joined clinic: ${clinicName}`);
-      }
-    } catch (error) {
-      setErrorMessage((error as Error).message);
-    } finally {
-      setIsLoading(false);
-    }
   };
 
   return (
